cf7342d62e19a6dae73494128f665c5233e5f2e2
Shara Shami <shara.alves@gmail.com>
2019-01-16 12:19:42 -0200

    trying to run the example but it is not finding fusiOld score, so I commented ust to see how it works without it

--- crisporEffScores.py
+++ crisporEffScores.py
@@ -762,7 +762,9 @@ def inList(l, name):
 
 # list of possible score names, by enzyme
 possibleScores = {
-    "spcas9" : ["fusi", "housden", "wang", "doench", "ssc",
+    "spcas9" : ["housden", "wang", "doench", "ssc",
+        "wuCrispr", "chariRank", "crisprScan", "aziInVitro", "ccTop", "oof"],
+    #"spcas9" : ["fusi", "housden", "wang", "doench", "ssc",
         "wuCrispr", "chariRank", "crisprScan", "aziInVitro", "ccTop", "oof"],
     "cpf1" : ["deepCpf1", "oof"],
     "sacas9" : ["najm", "oof"]
